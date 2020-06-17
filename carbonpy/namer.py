ded5351dd5bde70d377849d3e6057b88ab38e8b7
Harshil <37377066+harshil21@users.noreply.github.com>
2020-06-17 18:55:47 +0400

    merge master into tests (#4)

--- carbonpy/namer.py
+++ carbonpy/namer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 
 # Copyright © 2020 Harshil Mehta
 
@@ -210,23 +210,3 @@ class Namer(object):  # IUPAC Names for now only
             return None
         else:
             return lowest_back  # Can also return lowest_front(if compound is symmetrical)
-
-
-if __name__ == "__main__":
-    print("Type `/help` to get usage information.")
-
-    while True:
-        try:
-            compound_struct = input("Condensed structure > ").strip()
-            if compound_struct == "/help":
-                print(__doc__)
-            else:
-                compound = Namer(compound_struct)
-                print(f"\n{compound}\n{compound.molecular_formula()}\n{compound.analyser()}\n")
-
-        except EOFError:  # Ctrl-D on Windows
-            print("\nExiting...")
-            raise SystemExit
-
-        except ValencyError as e:
-            print(e)
